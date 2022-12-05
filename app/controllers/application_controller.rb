cccbce26dc182aecd4cddabdd0860d91ab5d4c97
alexgicimo <gakurualexgicimo@gmail.com>
2022-12-05 20:36:42 +0000

    Password Protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def create 
+    user = User.find_by(username: params[:username])
+
+    if user&.authenticate(username: params[:username])
+      session[:user_id] = user.id
+
+      render json: user ,status: :created
+    else
+      render json: { error: "Invalid username or password" }, status: :unauthorized
+    end
+  end
 end
